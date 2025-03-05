392b605232bc752b94fe7f14bf090bee096284b1
kdongsu5509 <kdongsu5509@gmail.com>
2025-03-05 10:53:49 +0900

    refactor(이미지 주소 불러오기): 서버 주소 비밀키로 변경
    서버 주소를 기존 Service 객체에 저장해놓았는데, 차후 보안 및 유지 보수 편리성을 위해 기본 URL 주소를 dotenv로 관리할 수 있도록 변경함

--- lib/DataLayer/Service/ImageService.dart
+++ lib/DataLayer/Service/ImageService.dart
@@ -1,6 +1,9 @@
 import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:knucl_face_analyzer_2025/DataLayer/Repository/ImageRepository.dart';
+import 'package:knucl_face_analyzer_2025/DataLayer/Repository/ImageRepositoryLocal.dart';
 import 'package:knucl_face_analyzer_2025/DataLayer/Service/my_dio.dart';
 
 /**
@@ -8,6 +11,9 @@ import 'package:knucl_face_analyzer_2025/DataLayer/Service/my_dio.dart';
  * @추상화 : 해당 프로젝트는 일회성이라 별도의 추상화 클래스 생성하지 않음.
  */
 class ImageService {
+
+  ImageRepository imageRepository = ImageRepositoryLocal();
+
   Dio dio = MyDio().get();
 
   Future<String> postImage(Uint8List encodedImage) {
@@ -15,13 +21,17 @@ class ImageService {
     MultipartFile file = MultipartFile.fromBytes(encodedImage, filename: "image.jpg");
 
     //send image to server
-    return dio.post("http://172.29.89.217:8080/image", data: FormData.fromMap({"img": file}))
+    return dio.post("${dotenv.env['baseUrl']}/image", data: FormData.fromMap({"img": file}))
       .then((response) => response.data);
   }
 
   Future<bool> deleteImage(int id) {
+
+    //get image address from local repository
+    String imgAddress = imageRepository.getImage(id);
+
     //delete image from server
-    return dio.delete("http://172.29.89.217:8080/image/$id")
+    return dio.delete("${dotenv.env['baseUrl']}/image", data: {"imgAddress": imgAddress})
       .then((response) => response.statusCode == 200);
   }
 }
--- lib/View/main.dart
+++ lib/View/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'home_view.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: "knuclFaceAnalyzer2025.env");
   runApp(ProviderScope(child: MyApp()),);
 }
 
