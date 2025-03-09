ee27cdc2264b5911f72e52fe62a6bb4e27775d08
kdongsu5509 <kdongsu5509@gmail.com>
2025-03-09 22:30:01 +0900

    refactor(project): 폴더 구조 변경
    main 함수를 mvvm 어디에도 포함하기 어려워 lib 레포 안의 최상단으로 배치

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'home_view.dart';
+import 'View/home_view.dart';
 
 Future main() async {
   await dotenv.load(fileName: "knuclFaceAnalyzer2025.env");
