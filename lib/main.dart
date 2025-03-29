febeb3b1d55fa5d0a252aaabbdede97a5d7cdd51
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-03-30 00:50:01 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'View/home_view.dart';
 
 Future main() async {
   await dotenv.load(fileName: "knuclFaceAnalyzer2025.env");
+  usePathUrlStrategy();
   runApp(ProviderScope(child: MyApp()),);
 }
 
